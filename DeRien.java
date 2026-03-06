894f6c8b2d6ab03f00e81c30f686f6e4a04815f5
NissrineELMNIAI <nissrineelmniai0@gmail.com>
2026-03-06 14:00:20 +0000

    first commit

---
+++ DeRien.java
@@ -0,0 +1,8 @@
+
+class DeRien {
+
+    public static void main(String[] args) {
+        System.out.println("Derien!");
+
+    }
+}
---
+++ Merci.java
@@ -0,0 +1,8 @@
+
+class Merci {
+
+    public static void main(String[] args) {
+        System.out.println("Merci!");
+
+    }
+}
